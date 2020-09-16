6e08a2da9f9b7154cc0cf02a9e9d3c8d479afb2a
Fredrik Fornwall <fredrik@fornwall.net>
2020-09-16 12:25:37 +0200

    Use other git command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -l
 
-find . | git check-ignore --stdin > /tmp/ignored-files
-if [ $? = 0 ]; then
+git ls-files -i --exclude-standard > /tmp/ignored-files
+if [ -s /tmp/ignored-files ]; then
 	echo "Files ignored by git exists in the repository:"
 	echo ""
 	cat /tmp/ignored-files
+	exit 1
 fi
 
 exit 0
